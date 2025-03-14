bb3dfcba06726e8ae3b9d6ab57ffb1281e8dedd5
dbyoung <dbyoung@sina.com>
2025-03-14 16:00:49 +0800

    update

--- src/004.bzip2.cmd
+++ src/004.bzip2.cmd
@@ -11,3 +11,21 @@ set BuildPath=%BuildPathX%\%~n0\%Platform1%
 :: cmake source
 set SpecialCMakeParam=""
 call %RootPath%src\vcmake.cmd %PackageName% %LibraryName% %ProjectName% %SourceCDX% %BuildPath% %SpecialCMakeParam%
+
+if exist %PCInstall%\bzip2.pc exit /b
+
+set msysVSSDKPath=%VSSDKPath:\=/%
+echo 
+(
+	echo prefix=%msysVSSDKPath%
+	echo exec_prefix=${prefix}
+	echo libdir=${exec_prefix}/lib
+	echo includedir=${prefix}/include
+	echo. 
+	echo Name: bzip2
+	echo Description: Lossless, block-sorting data compression
+	echo Version: 1.0.6
+	echo Libs: -L${libdir} -lbz2 -lzlib -llibpng -lbrotlidec -lbrotlicommon -liconv -llibcharset
+	echo Libs.private: -lzlib -llibpng -lbrotlidec -lbrotlicommon -liconv -llibcharset
+	echo Cflags: -I${includedir}
+)>%PCInstall%\bzip2.pc
--- src/026.fontconfig.cmd
+++ src/026.fontconfig.cmd
@@ -16,10 +16,9 @@ if exist "%VSSDKPath%\lib\%LibraryName%.lib" exit /b
 :: CMake fontconfig
 echo ¡ï¡ï¡ï¡ï¡ï CMake %PackageName% ¡ï¡ï¡ï¡ï¡ï
 call %RootPath%src\delbtp %BuildPath%
-%scoop%\apps\meson\current\meson setup -Dbuildtype=release -Ddefault_library=static -Ddebug=false -Db_vscrt=mt -Dtests=disabled -Dtools=disabled --prefix=%VSSDKPath% %SourceCDX% %BuildPath%
+%scoop%\apps\meson\current\meson setup -Dbuildtype=release -Ddefault_library=static -Ddebug=false -Ddoc=disabled -Db_vscrt=mt -Dtests=disabled -Dtools=disabled -Diconv=disabled --prefix=%VSSDKPath% %SourceCDX% %BuildPath%
 CD /D "%BuildPath%"
 %scoop%\apps\ninja\current\ninja
-%scoop%\apps\ninja\current\ninja install
 
 :: check build error
 if %errorlevel% NEQ 0 (
@@ -27,12 +26,28 @@ if %errorlevel% NEQ 0 (
 	pause
 )
 
-if exist "%VSSDKPath%\lib\fontconfig.lib" exit /b
+CD /D "%VSSDKPath%\lib"
+copy /Y /b "%BuildPath%\src\libfontconfig.a"  libfontconfig.lib
+copy /Y /b "%BuildPath%\src\libfontconfig.a"  fontconfig.lib
 
-CD /D %VSSDKPath%\lib
-copy /Y /b libfontconfig.a libfontconfig.lib
-copy /Y /b libfontconfig.a fontconfig.lib
-copy /Y /b libexpat.a      libexpat.lib
-copy /Y /b libexpat.a      expat.lib
-copy /Y /b libfreetype.a   libfreetype.lib
-copy /Y /b libfreetype.a   freetype.lib
+if not exist "%VSSDKPath%\include\fontconfig" (
+  CD /D "%VSSDKPath%\include"
+  MD fontconfig
+  CD /D fontconfig
+  copy /b /y "%SourceCDX%\fontconfig\fontconfig\*.h"
+)
+
+set msysVSSDKPath=%VSSDKPath:\=/%
+echo 
+(
+	echo prefix=%msysVSSDKPath%
+	echo includedir=${prefix}/include
+	echo libdir=${prefix}/lib
+	echo. 
+	echo Name: Fontconfig
+	echo Description: Font configuration and customization library
+	echo Version: 2.16.1
+	echo Libs: -L${libdir} -lfontconfig -llibxml2s -lfreetype -lbz2 -lzlib -llibpng -lbrotlidec -lbrotlicommon -liconv -llibcharset
+	echo Libs.private: -lfreetype -llibxml2s -lbz2 -lzlib -llibpng -lbrotlidec -lbrotlicommon -liconv -llibcharset
+	echo Cflags: -I${includedir}
+)>%PCInstall%\fontconfig.pc
