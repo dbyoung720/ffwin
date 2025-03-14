d8fb2e150d192ab3e70a5a104465f8a7a0fc4530
dbyoung <dbyoung@sina.com>
2025-03-14 17:04:33 +0800

    update

--- src/008.libpng.cmd
+++ src/008.libpng.cmd
@@ -19,3 +19,35 @@ del "%VSSDKPath%\bin\libpng16.dll"
 copy /b %VSSDKPath%\lib\libpng16_static.lib %VSSDKPath%\lib\libpng.lib
 copy /b %VSSDKPath%\lib\libpng16_static.lib %VSSDKPath%\lib\libpng16.lib
 copy /b %VSSDKPath%\lib\libpng16_static.lib %VSSDKPath%\lib\png16.lib
+
+set msysVSSDKPath=%VSSDKPath:\=/%
+echo 
+(
+	echo prefix=%msysVSSDKPath%
+	echo libdir=${exec_prefix}/lib
+	echo includedir=${prefix}/include/libpng16
+	echo.
+	echo Name: libpng
+	echo Description: Loads and saves PNG files
+	echo Version: 1.6.47
+	echo Requires.private: zlib
+	echo Libs: -L${libdir} -lpng16 -lzlib
+	echo Libs.private: -lzlib
+	echo Cflags: -I${includedir}
+)>%PCInstall%\libpng.pc
+
+echo 
+(
+	echo prefix=%msysVSSDKPath%
+	echo libdir=${exec_prefix}/lib
+	echo includedir=${prefix}/include/libpng16
+	echo.
+	echo Name: libpng
+	echo Description: Loads and saves PNG files
+	echo Version: 1.6.47
+	echo Requires.private: zlib
+	echo Libs: -L${libdir} -lpng16 -lzlib
+	echo Libs.private: -lzlib
+	echo Cflags: -I${includedir}
+)>%PCInstall%\libpng16.pc
+
