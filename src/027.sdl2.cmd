bbf36e699e5bc365c5277a312a42defb061876cc
dbyoung <dbyoung@sina.com>
2025-03-14 16:19:46 +0800

    update

--- src/027.sdl2.cmd
+++ src/027.sdl2.cmd
@@ -9,5 +9,5 @@ set SourceCDX=%SourcePath%\%packageName%
 set BuildPath=%BuildPathX%\%~n0\%Platform1%
 
 :: cmake source
-set SpecialCMakeParam=""
+set SpecialCMakeParam=" -DSDL_INSTALL_CMAKEDIR=%VSSDKLIB%\cmake "
 call %RootPath%src\vcmake.cmd %PackageName% %LibraryName% %ProjectName% %SourceCDX% %BuildPath% %SpecialCMakeParam%
